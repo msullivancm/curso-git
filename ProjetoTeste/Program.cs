b592c86edc7caf0fcf8314c080967a77a805b13a
msullivancm <sullivan@tibrasil.net>
2019-03-07 10:10:42 -0300

    Acrescentada a mensagem tenha um bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,7 +7,8 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia");
+
+            Console.WriteLine("Tenha um bom dia.");
             Console.ReadLine();
         }
     }
